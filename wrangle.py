4306a2676870ec245452381319eda1768a6123e0
Jonathan Garcia <jonathan.garcia.0136@gmail.com>
2023-09-19 00:45:20 -0500

    Clustering Explore Exercises

--- wrangle.py
+++ wrangle.py
@@ -23,28 +23,8 @@ def get_url(
     '''
     return f'mysql+pymysql://{user}:{password}@{host}/{schema}'
 
-def acquire_mall() -> pd.DataFrame:
-    '''
-    acquire_mall will use a local env.py
-    using pre-set credentials called user, password, and host
-    please make sure you have a properly formatted env.py
-    file in the same directory as this module
-    and that you have the access rights to mall_customers schema
-    
-    return: a single pandas dataframe
-    '''
-    # checking if the path to a cached version of mall data exists
-    if os.path.exists('mall_data.csv'):
-        # if it does, read it in
-        df = pd.read_csv('mall_data.csv')
-    else:
-        # else, we make a very basic select statement from mall_customers
-        # which only contains one table, called customers
-        query = 'SELECT * FROM customers'
-        url = get_url('mall_customers')
-        df = pd.read_sql(query, url)
-        df.to_csv('mall_data.csv', index=False)
-    return df
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
 
 def acquire_zillow() -> pd.DataFrame:
     '''
@@ -64,11 +44,17 @@ def acquire_zillow() -> pd.DataFrame:
         df.to_csv('zillow_data.csv', index=False)
     return df
 
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+
 def missing_by_col(df): 
     '''
     returns a single series of null values by column name
     '''
     return df.isnull().sum(axis=0)
+
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
 
 def missing_by_row(df) -> pd.DataFrame:
     '''
@@ -86,6 +72,9 @@ def missing_by_row(df) -> pd.DataFrame:
     count().reset_index().rename(columns={'index':'num_rows'})
     return rows_df
 
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+
 def get_fences(df, col, k=1.5) -> (float, float):
     '''
     get fences will calculate the upper and lower fence
@@ -100,6 +89,9 @@ def get_fences(df, col, k=1.5) -> (float, float):
     upper_bound = q3 + (k * iqr)
     lower_bound = q1 - (k * iqr)
     return lower_bound, upper_bound
+
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
 
 def report_outliers(df, k=1.5) -> None:
     '''
@@ -118,6 +110,8 @@ def report_outliers(df, k=1.5) -> None:
                 df[col] > upper_bound) | (df[col] < lower_bound)])
             print('----------')
 
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
 
 def summarize(df, k=1.5) -> None:
     '''
@@ -157,6 +151,9 @@ def summarize(df, k=1.5) -> None:
     print(report_outliers(df, k=k))
     print('======================\n======================')
 
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+
 def split_data(df, target=None) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     '''
     split_data will split data into train, validate, and test sets
@@ -178,6 +175,9 @@ def split_data(df, target=None) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
         stratify=target)
     return train, validate, test
 
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+
 def get_continuous_feats(df) -> list:
     '''
     find all continuous numerical features
@@ -190,6 +190,138 @@ def get_continuous_feats(df) -> list:
         if num_df[col].nunique() > 20:
             num_cols.append(col)
     return num_cols
+
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+
+def handle_missing_values(df, 
+                          prop_required_column, 
+                          prop_required_row):
+    '''
+    Utilizing an input proportion for the column and rows of DataFrame df,
+    drop the missing values per the axis contingent on the amount of data present.
+    '''
+    prop_missing_col = 1 - prop_required_column 
+    # multiply the axis with with the appropriate ratio
+    # this will return the number of rows that we want to reference
+    # for our dropna function
+    n_required_column = round(df.shape[0] * prop_required_column)
+    df = df.dropna(axis=1, thresh=n_required_column)
+    n_required_row = round(df.shape[1] * prop_required_row)
+    df = df.dropna(axis=0, thresh=n_required_row)
+    return df
+
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+
+def prep_zillow(
+    df, 
+    prop_required_row=0.75, 
+    prop_required_column=0.75
+    ) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
+    '''
+    prep mall will set the index of the customer id to the 
+    dataframe index, and will  scale continuous data in the df.
+    
+    return: a single, cleaned dataset.
+    '''
+    df = handle_missing_values(
+        df, 
+        prop_required_column=prop_required_column,
+        prop_required_row=prop_required_row
+    )
+    # take care of any duplicates:
+    df = df.drop_duplicates()
+    train, validate, test = split_data(df)
+    num_cols = get_continuous_feats(df)
+    # preprocessing:
+    #make a scaler:
+    scaler = MinMaxScaler()
+    scaled_cols = [col + '_scaled' for col in num_cols]
+    train[scaled_cols] = scaler.fit_transform(train[num_cols])
+    validate[scaled_cols] = scaler.transform(validate[num_cols])
+    test[scaled_cols] = scaler.transform(test[num_cols])
+    return train, validate, test
+
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+
+def wrangle_zillow(summarization=True,
+                   k=1.5,
+                   prop_required_column=0.75,
+                   prop_required_row=0.75) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
+    '''
+    wrangle_zillow will acquire and prepare zillow data
+    
+    if summarization is set to True, a console report 
+    of data summary will be output to the console.
+    
+    return: train, validate, and test data sets with scaled numeric information
+    '''
+    if summarization:
+        summarize(acquire_zillow(), k=k)
+    train, validate, test = prep_zillow(
+        acquire_zillow(),
+        prop_required_column=prop_required_column,
+        prop_required_row=prop_required_row
+    )
+    return train, validate, test
+
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
+
+def acquire_mall() -> pd.DataFrame:
+    '''
+    acquire_mall will use a local env.py
+    using pre-set credentials called user, password, and host
+    please make sure you have a properly formatted env.py
+    file in the same directory as this module
+    and that you have the access rights to mall_customers schema
+    
+    return: a single pandas dataframe
+    '''
+    # checking if the path to a cached version of mall data exists
+    if os.path.exists('mall_data.csv'):
+        # if it does, read it in
+        df = pd.read_csv('mall_data.csv')
+    else:
+        # else, we make a very basic select statement from mall_customers
+        # which only contains one table, called customers
+        query = 'SELECT * FROM customers'
+        url = get_url('mall_customers')
+        df = pd.read_sql(query, url)
+        df.to_csv('mall_data.csv', index=False)
+    return df
+
+def acquire_mall() -> pd.DataFrame:
+    '''
+    acquire_mall will use a local env.py
+    using pre-set credentials called user, password, and host
+    please make sure you have a properly formatted env.py
+    file in the same directory as this module
+    and that you have the access rights to mall_customers schema
+    
+    return: a single pandas dataframe
+    '''
+    # checking if the path to a cached version of mall data exists
+    if os.path.exists('mall_data.csv'):
+        # if it does, read it in
+        df = pd.read_csv('mall_data.csv')
+    else:
+        # else, we make a very basic select statement from mall_customers
+        # which only contains one table, called customers
+        query = 'SELECT * FROM customers'
+        url = get_url('mall_customers')
+        df = pd.read_sql(query, url)
+        df.to_csv('mall_data.csv', index=False)
+    return df
+
 
 def prep_mall(df) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     '''
@@ -226,53 +358,6 @@ def prep_mall(df) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     ]
     return train, validate, test
 
-def handle_missing_values(df, 
-                          prop_required_column, 
-                          prop_required_row):
-    '''
-    Utilizing an input proportion for the column and rows of DataFrame df,
-    drop the missing values per the axis contingent on the amount of data present.
-    '''
-    prop_missing_col = 1 - prop_required_column 
-    # multiply the axis with with the appropriate ratio
-    # this will return the number of rows that we want to reference
-    # for our dropna function
-    n_required_column = round(df.shape[0] * prop_required_column)
-    df = df.dropna(axis=1, thresh=n_required_column)
-    n_required_row = round(df.shape[1] * prop_required_row)
-    df = df.dropna(axis=0, thresh=n_required_row)
-    return df
-
-
-def prep_zillow(
-    df, 
-    prop_required_row=0.75, 
-    prop_required_column=0.75
-    ) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
-    '''
-    prep mall will set the index of the customer id to the 
-    dataframe index, and will  scale continuous data in the df.
-    
-    return: a single, cleaned dataset.
-    '''
-    df = handle_missing_values(
-        df, 
-        prop_required_column=prop_required_column,
-        prop_required_row=prop_required_row
-    )
-    # take care of any duplicates:
-    df = df.drop_duplicates()
-    train, validate, test = split_data(df)
-    num_cols = get_continuous_feats(df)
-    # preprocessing:
-    #make a scaler:
-    scaler = MinMaxScaler()
-    scaled_cols = [col + '_scaled' for col in num_cols]
-    train[scaled_cols] = scaler.fit_transform(train[num_cols])
-    validate[scaled_cols] = scaler.transform(validate[num_cols])
-    test[scaled_cols] = scaler.transform(test[num_cols])
-    return train, validate, test
-
 
 def wrangle_mall(summarization=True, k=1.5) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     '''
@@ -286,25 +371,4 @@ def wrangle_mall(summarization=True, k=1.5) -> (pd.DataFrame, pd.DataFrame, pd.D
     if summarization:
         summarize(acquire_mall(), k=k)
     train, validate, test = prep_mall(acquire_mall())
-    return train, validate, test
-
-def wrangle_zillow(summarization=True,
-                   k=1.5,
-                   prop_required_column=0.75,
-                   prop_required_row=0.75) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
-    '''
-    wrangle_zillow will acquire and prepare zillow data
-    
-    if summarization is set to True, a console report 
-    of data summary will be output to the console.
-    
-    return: train, validate, and test data sets with scaled numeric information
-    '''
-    if summarization:
-        summarize(acquire_zillow(), k=k)
-    train, validate, test = prep_zillow(
-        acquire_zillow(),
-        prop_required_column=prop_required_column,
-        prop_required_row=prop_required_row
-    )
     return train, validate, test
